df1ceb78a5a113a9b5a4d8e6ce84f1476b80747f
Yong-il Lee <link@openable.net>
2017-01-16 05:40:04 +0900

    Program.cs에 EyeX Framework 관련 설정

--- ImplicitTest/Program.cs
+++ ImplicitTest/Program.cs
@@ -1,22 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+
+using EyeXFramework.Forms;
 
 namespace ImplicitTest
 {
     static class Program
     {
+        private static FormsEyeXHost _eyeXHost = new FormsEyeXHost();
+
         /// <summary>
         /// 해당 응용 프로그램의 주 진입점입니다.
         /// </summary>
+        
+        public static FormsEyeXHost EyeXHost
+        {
+            get { return _eyeXHost; }
+        }
+
         [STAThread]
         static void Main()
         {
+            _eyeXHost.Start();
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Main());
+
+            _eyeXHost.Dispose();
         }
     }
 }
