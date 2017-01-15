158e2baf3015cb1a0b6a63125a1d81615f83866d
Yong-il Lee <link@openable.net>
2017-01-16 08:34:30 +0900

    Word Class 생성

---
+++ ImplicitTest/Model/Word.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImplicitTest.Model
+{
+    class Word : System.Windows.Forms.Button
+    {
+        private double gazeTime;
+        private string text;
+
+        public Word()
+        {
+            gazeTime = 0.0;
+        }
+
+        public Word(string text)
+        {
+            gazeTime = 0.0;
+            this.text = text;
+        }
+
+    }
+}
