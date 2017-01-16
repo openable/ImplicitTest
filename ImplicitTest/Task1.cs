ec1ac13105efc89344904ca814f380887a6d9f41
Yong-il Lee <link@openable.net>
2017-01-16 14:32:10 +0900

    Task1 클래스 최대 반복 제한하는 max 변수 추가, initStimulus 함수로 버튼 배열 모듈화, formClosing 함수와 word_Click 함수 내용 조절

--- ImplicitTest/Task1.cs
+++ ImplicitTest/Task1.cs
@@ -22,6 +22,7 @@ namespace ImplicitTest
         private Word stimulus;
         private Word[] words = new Word[15];
         private int order = 1;
+        private int max = 4;
 
         public Task1()
         {
@@ -31,20 +32,25 @@ namespace ImplicitTest
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.WindowState = FormWindowState.Maximized;
 
+            initStimulus();
+
+            lightlyFilteredGazeDataStream.Next += gazeDataStreamHandler;
+        }
+
+        private void initStimulus()
+        {
             taskNum.Text = "문항1-" + order;
-            stimulus = new Word("연상단어");
+            stimulus = new Word("연상단어" + order);
             stimulus.SetBounds((int)Setting.cStimulus.X, (int)Setting.cStimulus.Y, (int)Setting.sStimulus.X, (int)Setting.sStimulus.Y);
             this.Controls.Add(stimulus);
 
             for (int i = 0; i < 15; i++)
             {
-                words[i] = new Word("단어 " + (i+1));
+                words[i] = new Word("단어 " + order + "-" + (i + 1));
                 words[i].SetBounds((int)Setting.cWord[i].X, (int)Setting.cWord[i].Y, (int)Setting.sWord.X, (int)Setting.sWord.Y);
                 words[i].Click += new System.EventHandler(this.word_Click);
                 this.Controls.Add(words[i]);
             }
-            
-            lightlyFilteredGazeDataStream.Next += gazeDataStreamHandler;
         }
 
         private void gazeDataStreamHandler(object sender, GazePointEventArgs e)
@@ -69,16 +75,29 @@ namespace ImplicitTest
         private void formClosing(object sender, FormClosingEventArgs e)
         {
             lightlyFilteredGazeDataStream.Next -= gazeDataStreamHandler;
-
-            for (int i = 0; i < 15; i++)
-            {
-                Console.WriteLine("{0}\t{1}", words[i].Text, words[i].gazeTime);
-            }
         }
 
         private void word_Click(object sender, EventArgs e)
         {
-            this.Close();
+            Graphics gr = this.CreateGraphics();
+            gr.Clear(Color.White);
+            gr.Dispose();
+
+            this.Controls.Remove(stimulus);
+
+            for (int i = 0; i < 15; i++)
+            {
+                this.Controls.Remove(words[i]);
+                Console.WriteLine("{0}\t{1}", words[i].Text, words[i].gazeTime);
+            }
+
+            if (order < max)
+            {
+                order = order + 1;
+                initStimulus();
+            }
+            else
+                this.Close();
         }
     }
 }
