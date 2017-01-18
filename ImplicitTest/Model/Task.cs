d695805ef063e3d635ff176ca83fe0b092170fa6
Yong-il Lee <link@openable.net>
2017-01-19 07:48:41 +0900

    Task 클레스 추가 및 정의

---
+++ ImplicitTest/Model/Task.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ImplicitTest.Model
+{
+    class Task
+    {
+        public int type;
+        public string stimulus;
+        public ArrayList choice;
+    }
+}
