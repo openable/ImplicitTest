ec508c872a43441f24bd7e8482fc23f6117d7993
Yong-il Lee <link@openable.net>
2017-01-23 02:03:44 +0900

    Main 클래스 설정 디자인 변경

--- ImplicitTest/Main.Designer.cs
+++ ImplicitTest/Main.Designer.cs
@@ -35,19 +35,27 @@
             this.caliBtn = new System.Windows.Forms.Button();
             this.screenBtn = new System.Windows.Forms.Button();
             this.startBtn = new System.Windows.Forms.Button();
-            this.settingBtn = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(75, 43);
+            this.textBox4.Location = new System.Drawing.Point(75, 47);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(110, 21);
             this.textBox4.TabIndex = 12;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(75, 16);
+            this.textBox1.Location = new System.Drawing.Point(75, 17);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(110, 21);
             this.textBox1.TabIndex = 11;
@@ -55,7 +63,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(23, 46);
+            this.label4.Location = new System.Drawing.Point(23, 50);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(45, 12);
             this.label4.TabIndex = 10;
@@ -64,7 +72,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(36, 19);
+            this.label1.Location = new System.Drawing.Point(36, 20);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(33, 12);
             this.label1.TabIndex = 9;
@@ -72,7 +80,7 @@
             // 
             // caliBtn
             // 
-            this.caliBtn.Location = new System.Drawing.Point(56, 138);
+            this.caliBtn.Location = new System.Drawing.Point(56, 120);
             this.caliBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.caliBtn.Name = "caliBtn";
             this.caliBtn.Size = new System.Drawing.Size(109, 27);
@@ -83,7 +91,7 @@
             // 
             // screenBtn
             // 
-            this.screenBtn.Location = new System.Drawing.Point(56, 103);
+            this.screenBtn.Location = new System.Drawing.Point(56, 80);
             this.screenBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.screenBtn.Name = "screenBtn";
             this.screenBtn.Size = new System.Drawing.Size(109, 27);
@@ -94,7 +102,7 @@
             // 
             // startBtn
             // 
-            this.startBtn.Location = new System.Drawing.Point(56, 208);
+            this.startBtn.Location = new System.Drawing.Point(56, 320);
             this.startBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.startBtn.Name = "startBtn";
             this.startBtn.Size = new System.Drawing.Size(109, 27);
@@ -103,22 +111,91 @@
             this.startBtn.UseVisualStyleBackColor = true;
             this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
             // 
-            // settingBtn
+            // groupBox1
             // 
-            this.settingBtn.Location = new System.Drawing.Point(56, 173);
-            this.settingBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.settingBtn.Name = "settingBtn";
-            this.settingBtn.Size = new System.Drawing.Size(109, 27);
-            this.settingBtn.TabIndex = 16;
-            this.settingBtn.Text = "실험 환경 설정";
-            this.settingBtn.UseVisualStyleBackColor = true;
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 170);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 130);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "실험 환경 설정";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "시작 위치:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 12);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "글씨 크기:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(10, 90);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 12);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "시선 표시:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(73, 27);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 21);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(73, 57);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 21);
+            this.textBox3.TabIndex = 4;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(73, 88);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(35, 16);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "예";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(114, 88);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(59, 16);
+            this.radioButton2.TabIndex = 6;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "아니오";
+            this.radioButton2.UseVisualStyleBackColor = true;
             // 
             // Main
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(224, 261);
-            this.Controls.Add(this.settingBtn);
+            this.ClientSize = new System.Drawing.Size(224, 361);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.caliBtn);
             this.Controls.Add(this.screenBtn);
             this.Controls.Add(this.startBtn);
@@ -129,6 +206,8 @@
             this.Name = "Main";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "WAT";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -143,7 +222,14 @@
         private System.Windows.Forms.Button caliBtn;
         private System.Windows.Forms.Button screenBtn;
         private System.Windows.Forms.Button startBtn;
-        private System.Windows.Forms.Button settingBtn;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
